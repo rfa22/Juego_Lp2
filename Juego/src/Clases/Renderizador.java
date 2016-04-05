d944393f14c04169f76ce1d41f24beedbeb20840
alulab14 <alulab14@V20827.lab.inf.pucp.edu.pe>
2016-04-05 10:22:21 -0500

    Se creo imprimeMenu y se está realizando imprimeInstrucciones

--- Juego/src/Clases/Renderizador.java
+++ Juego/src/Clases/Renderizador.java
@@ -10,28 +10,38 @@ package Clases;
  * @author alulab14
  */
 public class Renderizador {
-    
-    
-    public Renderizador(){
-        
-    }    
-    
-    public void imprimeMapa(Mapa m,Personaje a,Personaje b){
+
+    public Renderizador() {
+
+    }
+
+    public void imprimeMapa(Mapa m, Personaje a, Personaje b) {
         int fil = m.getAlto();
         int col = m.getAncho();
-                                                        
+
         m.getCelda(a.getPosI(), a.getPosJ()).setContenido('A');
         m.getCelda(b.getPosI(), b.getPosJ()).setContenido('B');
-        for(int i=0;i<10;i++){
+        for (int i = 0; i < 10; i++) {
             m.getCelda(0, i).setContenido('♥');
             m.getCelda(0, i).imprimeCelda();
         }
-        System.out.println("\n");        
-        for(int i=1; i<col; i++){
-            for(int j=0; j<fil; j++){                           
+        System.out.println("\n");
+        for (int i = 1; i < col; i++) {
+            for (int j = 0; j < fil; j++) {
                 m.getCelda(i, j).imprimeCelda();
             }
             System.out.println();
-        }                
-    }    
+        }
+    }
+
+    public void imprimeMenu() {
+        System.out.println("Elija una opción:");
+        System.out.println("1.- Iniciar el juego");
+        System.out.println("2.- Salir del juego");
+    }
+    
+    public void imprimeInstrucciones(){
+        System.out.println("Instrucciones:");
+        System.out.println("Mover:");
+    }
 }
