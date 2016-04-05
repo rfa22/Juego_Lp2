23e2a771dadbd904f4189f8306fa5d247635c2e1
alulab14 <alulab14@V20827.lab.inf.pucp.edu.pe>
2016-04-05 10:46:49 -0500

    Constructor Mapa modificado

--- Juego/src/Clases/Mapa.java
+++ Juego/src/Clases/Mapa.java
@@ -16,22 +16,36 @@ public class Mapa {
     private final Celda[][] mapa;
     
     public Mapa(int an, int al){
-        ancho = an; //16
-        alto = al;  //13
+        ancho = an; //18
+        alto = al;  //16
         
         //creando mapa
-        mapa = new Celda[ancho][alto];
+        mapa = new Celda[alto][ancho];
         for(int i=0;i<10;i++){
             mapa[0][i] = new Celda('♥');
         }
-        for(int i=1; i<ancho;i++){
-            for(int j=0; j<alto; j++){
+        
+        for(int i=1; i<alto;i++) //Marco del mapa lado izquierdo
+            mapa[i][0] = new Celda('█');
+        
+        for(int i=1; i<alto;i++) //Marco del mapa lado derecho
+            mapa[i][17] = new Celda('█');
+        
+        for(int j=1; j<ancho-1;j++) //Marco del mapa lado arriba
+            mapa[1][j] = new Celda('▀');
+        
+        for(int j=1; j<ancho-1;j++) //Marco del mapa lado abajo
+            mapa[15][j] = new Celda('▀');
+        
+        
+        for(int i=2; i<alto-1;i++){ //parte del mapa donde se mueve los jugadores
+            for(int j=1; j<ancho-1; j++){
                 mapa[i][j] = new Celda('#');                                
             }
-        }
+        }             
                                         
-        for(int j=0; j<alto; j++) //division de las pantallas de A y B
-           mapa[7][j].setContenido('-');
+        for(int j=0; j<ancho; j++) //division de las pantallas de A y B
+           mapa[8][j].setContenido('-');
         
 //        mapa[3][0].setContenido('A'); //posicionar al personaje A
 //        mapa[9][0].setContenido('B'); //posicionar al personaje B               
